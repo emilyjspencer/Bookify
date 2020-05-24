87a8b89239e7aba0d566879571a8d360f994626c
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-24 14:21:40 +0100

    Adds EditBook component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import CreateBook from './components/CreateBook';
+import EditBook from './components/EditBook'
 
 function App() {
   return (
     <div className="App">
       < CreateBook />
+      < EditBook />
     </div>
   );
 }
