6e3471bd4d50ad4f73635d931c32f7ed12361603
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-24 14:19:33 +0100

    Adds CreateBook component

---
+++ src/components/CreateBook.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CreateBook extends Component {
+  render() {
+    return (
+      <div>
+        <p>Create Book</p>
+      </div>
+    );
+  }
+}
+
+export default CreateBook;
