c6e1b81cf68bbe7a9bca93e4d69c4a9a255d3f44
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-24 16:11:33 +0100

    Adds a form to the AddBook component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import AddBook from './components/AddBook';
 import EditBook from './components/EditBook';
@@ -21,8 +20,9 @@ function App() {
       <header className="App-header">
         <Navbar bg="dark" variant="dark">
           <Container>
+
             <Navbar.Brand>
-              <Link to={'/AddBook'} className='nav-link'>
+              <Link to={'/'} className='nav-link'>
                 Bookify
               </Link>
             </Navbar.Brand>
@@ -41,21 +41,23 @@ function App() {
           </Container>
         </Navbar>
       </header>
+
       <Container>
         <Row>
           <Col md={12}>
             <div className="wrapper">
               <Switch>
-                <Route exact path='/' component={AddBook} />
+                <Route exact path='/' />
+                <Route path='/Addbook' component={AddBook} />
                 <Route path='/EditBook/:id' component={EditBook} />
                 <Route path='/BookList' component={BookList} />
-                  </Switch>
-    </div>
-    </Col>
-    </Row>
+              </Switch>
+             </div>
+          </Col>
+       </Row>
     </Container>
     </div>
-    </Router>
+  </Router>
   );
 }
 
--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,10 +1,33 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class AddBook extends Component {
+
   render() {
     return (
-      <div>
-        <p>Add Book</p>
+      <div className='form-wrapper'>
+        <Form>
+          <Form.Group controlId="Title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+        <Form.Group controlId="Author">
+          <Form.Label>Author</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group controlId="Genre">
+          <Form.Label>Genre</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group controlId="Isbn">
+          <Form.Label>Isbn</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Button variant="info" size="mg" block="block" type="submit">
+          Add Book
+        </Button>
+       </Form>
       </div>
     );
   }
