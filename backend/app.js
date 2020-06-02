923047e1ffe7fc7c9a791ad666bf97792b7c08bd
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 01:19:46 +0100

    Adds server file

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+let express = require('express');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let dbConfig = require('./database/db');
+
+
+const bookRoute = require('../backend/routes/book.route')
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log('Connection to database established')
+},
+  error => {
+    console.log('Connection to database not established : ' + error)
+  }
+)
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cors());
+app.use('/books', bookRoute)
+
+
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
